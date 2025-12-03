fd76802396ceb37ca1cb4c01b6d1958c6e06e5db
Marien JOANNY <77669242+marienjoanny@users.noreply.github.com>
2025-12-03 14:26:39 +0100

    Update index-real.php
    init consent

--- public/index-real.php
+++ public/index-real.php
@@ -112,14 +112,16 @@
 
     <!-- Scripts externes -->
     <script src="https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js"></script>
-   <script src="https://cdn.jsdelivr.net/npm/tracking/build/tracking-min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/tracking/build/data/face-min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/tracking/build/tracking-min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/tracking/build/data/face-min.js"></script>
+
     <!-- Scripts internes -->
     <script src="/js/utilities.js"></script>
     <script src="/js/ui-enhancements.js"></script>
     <script src="/js/camera.js"></script>
-    <script src="/js/face-visible.js"></script>
+    <!-- Charger match.js AVANT face-visible.js -->
     <script src="/js/match.js"></script>
+    <script src="/js/face-visible.js"></script>
     <script src="/js/signalement.js"></script>
     <script type="module" src="/app-lite.js"></script>
     
