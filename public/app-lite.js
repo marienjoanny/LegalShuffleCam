ae8867efbc157aa21a30e20f10cd2ce049d039e8
Marien JOANNY <77669242+marienjoanny@users.noreply.github.com>
2025-12-03 10:37:03 +0100

    Update app-lite.js
    debug peerjs

--- public/app-lite.js
+++ public/app-lite.js
@@ -1,12 +1,18 @@
+// app-lite.js
 import { initMatch, nextMatch, bindMatchEvents } from "/js/match.js";
 import { listCameras, startCamera } from "/js/camera.js";
 import { initFaceDetection, stopFaceDetection } from "/js/face-visible.js";
+import { showTopbarLog } from "/js/utilities.js";
 
 window.addEventListener('DOMContentLoaded', () => {
+  // 🎥 Liste des caméras disponibles
   listCameras();
+
+  // 🔗 Initialisation PeerJS + Match
   initMatch();
   bindMatchEvents();
 
+  // 🎛️ Sélecteur de caméra
   const select = document.getElementById('cameraSelect');
   select.addEventListener('change', () => {
     const deviceId = select.value;
