100624788d288424c692ff7d02a31e20543e9a86
Marien Joanny <marienjoanny@gmail.com>
2026-01-21 13:47:59 +0000

    🔒 SECURITY: Ignore config.php and cookies.txt from repository

--- backups-clean-20251115/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$config = array();
-
-// provide the access information, find this credentials in your partner account
-$config['partnerId'] = 592; // remplace par ton vrai ID
-$config['cipherKey'] = '502016cd09a61e4f3797dde30fc6499ab1456648ddcd83646735fe0b2d0c92a6';
-$config['hmacKey']   = '67f652fc33b10bef0e2f90cb41b10be5dd7e94d5c8c16cee6d8f6e3a1caaa4e464498aab495591e37acf3283c4367c2c74fe5210501d900af6ec5679bf1743f24300520a1bf6d30b351008bde59d0a430fd7b2e0a1a5d2b31ea7ed6ad390f1679362af7efe8b9b884300f08841c7e7011853d0078514e87d67a1cd67cbd716db';
--- backups-clean-20251115/public/clear-cookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-setcookie("age_verified", "", [
-    "expires" => time() - 3600,
-    "path" => "/",
-    "secure" => true,
-    "httponly" => true,
-    "samesite" => "Lax",
-]);
-echo "Cookie 'age_verified' supprimé. Rechargez la page d'accueil pour relancer Go.cam.";
